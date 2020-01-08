73d9a0914a446fc91b060f3575875e040b5e5e72
IDEA on T3600 <zexa91x0@mail.ru>
2020-01-08 15:51:27 +0500

    Lesson 07: поправил автоматизацию пути к базе данных. ## Основной функционал: 1. Регистрация и аутентификация на сервере + 2. Передача файлов с клиента на сервер, 3. скачивание файлов с сервера + 4. Просмотр списка файлов на сервере + 5. Переименование, удаление файлов на сервере + 6. Файлы хранятся на сервере в папках, названия которых соответствуют логину пользователя 7. * разработать jFX клиента
    ## Задача
    6. Файлы хранятся на сервере в папках, названия которых соответствуют логину пользователя

--- 15_GB_DropBox_SharedClasses/src/main/java/SettingsServer.java
+++ 15_GB_DropBox_SharedClasses/src/main/java/SettingsServer.java
@@ -6,12 +6,12 @@ public class SettingsServer {
     public static int HOST_PORT = 8189;
     public final static Path SERVER_PATH = Paths.get(System.getProperty("user.dir").concat("\\StorageRemote")).toAbsolutePath();
     public static Integer MAX_OBJECT_SIZE = 2047 * 1024 * 1024;
-    public final static String DB_DESTINATION = "D:\\Skills\\_Repository\\15_GB_DropBox\\15_GB_DropBox_Server\\src\\main\\resources\\DB\\usersDB.db";
+    public final static String DB_DESTINATION = Paths.get(System.getProperty("user.dir").concat("\\15_GB_DropBox_Server\\src\\main\\resources\\DB\\usersDB.db")).toAbsolutePath().toString();
 
 
     public static void main(String[] args) {
 
-        System.out.println(SERVER_PATH);
+        System.out.println(DB_DESTINATION);
 
     }
 
